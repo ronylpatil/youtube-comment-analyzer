0837e24b899c6bbfa17eeffcc58e31f903fe6dff
ronilpatil <ronilpatil2000@gmail.com>
2024-11-10 19:25:43 +0530

    model tunning expermentation with mlflow

--- src/models/tune_model.py
+++ src/models/tune_model.py
@@ -1,7 +1,11 @@
 import yaml
 import optuna  # type: ignore
+import joblib
+import mlflow
+import dagshub  # type: ignore
 import pathlib
 import lightgbm as lgbm  # type: ignore
+from datetime import datetime
 from functools import partial
 from src.logger import infologger
 from sklearn.metrics import accuracy_score
@@ -9,6 +13,7 @@ from src.features.build_features import loadData
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
 from src.visualization.visualize import conf_matrix
+from mlflow.models.signature import infer_signature
 from sklearn.model_selection import train_test_split
 from src.models.train_model import feature_extraction
 
@@ -21,11 +26,11 @@ def objective_lgbm(trial, x_train, y_train) -> float:
     data_sample_strategy = trial.suggest_categorical(
         "data_sample_strategy", ["bagging", "goss"]
     )
-    n_estimators = trial.suggest_int("n_estimators", 100, 800, step=20)
-    learning_rate = trial.suggest_float("learning_rate", 0.1, 0.3, step=0.01)
-    max_depth = trial.suggest_int("max_depth", 3, 12)
-    top_rate = trial.suggest_float("top_rate", 0.2, 0.6, step=0.1)
-    other_rate = trial.suggest_float("other_rate", 0.1, 0.5, step=0.1)
+    n_estimators = trial.suggest_int("n_estimators", 100, 500, step=10)
+    learning_rate = trial.suggest_float("learning_rate", 0.1, 0.4, step=0.1)
+    max_depth = trial.suggest_int("max_depth", 3, 10)
+    top_rate = trial.suggest_float("top_rate", 0.2, 0.4, step=0.1)
+    other_rate = trial.suggest_float("other_rate", 0.1, 0.4, step=0.1)
     # max_conflict_rate = trial.suggest_float("max_conflict_rate", 0.1, 0.5, step=0.1)
 
     # Create the RandomForestClassifier with suggested hyperparameters
@@ -56,14 +61,22 @@ def main() -> None:
 
     params = yaml.safe_load(open(f"{home_dir}/params.yaml", encoding="utf-8"))
 
+    dagshub.init(
+        repo_owner=params["train_model"]["repo_owner"],
+        repo_name=params["train_model"]["repo_name"],
+        mlflow=True,
+    )
+
     clean_df = loadData(f"{home_dir}/data/processed/clean_data.csv").dropna()
     # split the data
     X_train, X_test, y_train, y_test = train_test_split(
         clean_df["clean_text"],
         clean_df["category"],
         test_size=params["train_model"]["test_size"],
+        stratify=clean_df["category"],
+        random_state=42
     )
-    
+
     # convert [-1,0,1] to [0,1,2]
     target_map = {-1: 0, 0: 1, 1: 2}
     y_train = y_train.map(target_map).astype(float)
@@ -89,24 +102,85 @@ def main() -> None:
         direction="maximize", sampler=optuna.samplers.TPESampler()
     )  # We aim to maximize accuracy
     study.optimize(
-        partial(objective_lgbm, x_train=X_train_vect, y_train=y_train), n_trials=1
+        partial(objective_lgbm, x_train=X_train_vect, y_train=y_train), n_trials=params["tune_model"]["n_trials"]
     )
 
+  
+
     # training model with optimized hyperparameters
-    best_model = lgbm.LGBMClassifier(**study.best_trial.params)
+    best_model = lgbm.LGBMClassifier(
+        **study.best_trial.params, objective="multiclass", num_class=3
+    )
     best_model.fit(X_train_vect, y_train)
     y_pred = best_model.predict(X_test_vect)
 
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy}\n")
-
-    classification_rep = classification_report(
-        y_test, y_pred, output_dict=True, zero_division=0
+    curr_time = datetime.now().strftime("%d%m%y-%H%M%S")
+    pathlib.Path.mkdir(
+        pathlib.Path(f"{home_dir}/models/fine_tunned/{curr_time}"),
+        parents=True,
+        exist_ok=True,
     )
-    print(f"Classification Report: \n{classification_rep}\n")
+    joblib.dump(
+        best_model,
+        filename=f"{home_dir}/models/fine_tunned/{curr_time}/tunned_model.joblib",
+    )
 
-    cm_path = conf_matrix(y_test, y_pred, path=f"{home_dir}/figures")
-    print(f"Confusion Matrix Path: {cm_path}")
+    # set mlflow experiment name
+    mlflow.set_experiment(params["tune_model"]["experiment_name"])
+    experiment_description = params["tune_model"]["experiment_description"]
+    mlflow.set_experiment_tag("mlflow.note.content", experiment_description)
+
+    with mlflow.start_run() as run:
+        # set tags for the experiment
+        mlflow.set_tag(
+            "mlflow.runName",
+            "tunned_model_lightgbm",
+        )
+        # set particular experiment description
+        mlflow.set_tag(
+            "mlflow.note.content",
+            f"lightgbm model with bow, ngram_range=bigram, max_features=10000, n_trials={params['tune_model']['n_trials']}",
+        )
+
+        # experiment tag
+        mlflow.set_tag("experiment_type", params["tune_model"]["experiment_name"])
+        mlflow.set_tag("model", "lightgbm")
+        mlflow.set_tag("optimizer", "optuna")
+        mlflow.set_tag("n_trials", params["tune_model"]["n_trials"])
+
+        # log vectorizer parameters
+        mlflow.log_param("vectorizer_type", "bow")
+        mlflow.log_param("ngram_range", "bigram")
+        mlflow.log_param("vectorizer_max_features", 10000)
+        mlflow.log_params(study.best_trial.params)
+
+
+        model_signature = infer_signature(X_train_vect, y_train)
+
+        # log accuracy
+        accuracy = accuracy_score(y_test, y_pred)
+        mlflow.log_metric("accuracy", round(accuracy, 3))
+
+        # log classification report
+        classification_rep = classification_report(
+            y_test, y_pred, output_dict=True, zero_division=0
+        )
+        for label, metrics in classification_rep.items():
+            if isinstance(metrics, dict):
+                for metric, value in metrics.items():
+                    mlflow.log_metric(f"{label}_{metric}", value)
+
+        # log confusion matrix
+        cm_path = conf_matrix(y_test, y_pred, path=f"{home_dir}/figures")
+        mlflow.log_artifact(cm_path, "confusion_matrix")
+
+        # log the model
+        mlflow.sklearn.log_model(
+            best_model,
+            f"best_model_optuna",
+            signature=model_signature,
+        )
+        infologger.info("model successfully logged under mlflow")
 
 
 if __name__ == "__main__":
