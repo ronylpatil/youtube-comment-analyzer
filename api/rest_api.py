049625d4bedee56fc485102b63cbd9562bfedb08
ronilpatil <ronilpatil2000@gmail.com>
2024-11-13 10:06:11 +0530

    implement api with redis cache mechanism

---
+++ api/rest_api.py
@@ -0,0 +1,121 @@
+import json
+import yaml  # type: ignore
+import redis  # type: ignore
+import joblib  # type: ignore
+import logging
+import hashlib
+import pathlib
+from fastapi import FastAPI  # type: ignore
+from pydantic import BaseModel  # type: ignore
+from fastapi.responses import JSONResponse  # type: ignore
+from fastapi.middleware.cors import CORSMiddleware  # type: ignore
+from src.features.build_features import preprocessText
+
+# Create a custom log format
+log_format = "%(levelname)s:     %(message)s"
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format=log_format  # Apply the custom format here
+)
+logger = logging.getLogger(__name__)
+
+# load password from secrets.yaml
+curr_dir = pathlib.Path(__file__)
+home_dir = curr_dir.parent.parent.as_posix()
+key = yaml.safe_load(
+    open(f"{pathlib.Path(__name__).parent.parent.as_posix()}/secrets.yaml")
+)["redis_key"]
+
+app = FastAPI()
+
+# load ml model and vectorizer
+try:
+    model = joblib.load(
+        f"{home_dir}/models/fine_tunned/121124-220055/tunned_model.joblib"  # inout: 5k
+    )
+    vectorizer = joblib.load(f"{home_dir}/vectorizer/bow/bow_bigram_5000.joblib")
+except Exception as e:
+    logger.error(
+        f"unable to load model or vectorizer, check the potential issue. error: {e}"
+    )
+
+# CORS Middleware allow FastAPI to filter and control incomming requests from different origin (ex. frontend)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# connecting with redis db
+rd = redis.Redis(
+    host="clever-mayfly-27272.upstash.io", port=6379, password=key, ssl=True
+)
+
+
+# define a Pydantic model for the prediction request
+class PredictionRequest(BaseModel):
+    data: str
+
+# define a Pydantic model for the prediction response
+class PredictionResponse(BaseModel):
+    comment: str
+    sentiment: float
+
+def get_cache_key(data: str) -> str:
+    return hashlib.sha256(data.encode()).hexdigest()
+
+async def predict(data: str):
+    # use ml model for prediction
+    clean_data = preprocessText(data)
+    if clean_data == "":
+        logger.info("comment is neutral")
+        return 1.0
+
+    try:
+        vect_data = vectorizer.transform([clean_data]).toarray()
+        logger.info("data vectorized successfully")
+        output = model.predict(vect_data)[0]
+        logger.info(f"model prediction: {output}")
+        return output
+    except Exception as e:
+        logger.critical(
+            f"unable to vectorize or predict the sentiment, check async predict for potential issue. error: {e}"
+        )
+
+
+# function to get cached prediction from redis
+# define the prediction endpoint
+@app.post("/sentiment", response_model=PredictionResponse)
+async def prediction_endpoint(request: PredictionRequest):
+    logger.info(request)
+    cache_key = get_cache_key(request.data)
+    try: 
+        cache = rd.get(cache_key)
+    except Exception as e:
+        logger.error(f"Error accessing Redis: {e}")
+        # Handle the error gracefully (e.g., return a default value or proceed with a model prediction)
+        return JSONResponse({"error": "Redis error"})
+
+    if cache:
+        logger.info(f"cache hit!")
+        return json.loads(cache)
+    else:
+        logger.info(f"cache miss!, computing prediction...")
+        prediction = await predict(request.data)
+        r = {"comment": request.data, "sentiment": prediction}
+        rd.set(cache_key, json.dumps(r))
+        return JSONResponse(r)
+
+
+@app.get("/")
+def read_root():
+    return "Hello World!"
+
+
+# [it will look for changes in whole project directory, to limit this score use --reload-dir ./dir_name]
+# server cmd: uvicorn api.rest_api:app --reload --reload-dir ./api --host 127.0.0.1 --port 8000
+# install "httpie" to access API through "http :8000" cmd.
